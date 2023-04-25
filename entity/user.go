eb3249aaeb183eefce3573a3882b00e93e5a4216
fathanazka354 <fazka313@gmail.com>
2023-04-25 16:37:24 +0700

    first commit

--- entity/user.go
+++ entity/user.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	Id        int       `json:"id" validate:"required"`
 	Username  int       `json:"username" validate:"required"`
-	Email     int       `json:"email" validate:"required,email"`
+	Email     int       `json:"email" validate:"required,email,unique"`
 	Password  int       `json:"password" validate:"required,min=6,max=255"`
 	Age       int       `json:"age" validate:"required,numeric"`
 	CreatedAt time.Time `json:"createdAt"`
